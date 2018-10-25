01842e9f6c65d8a2fd21397e86d74a7012002ba6
Farhan Attamimi <farhan@sourcegraph.com>
2018-10-25 11:03:02 -0700

    Go imports extension

--- src/extension.ts
+++ src/extension.ts
@@ -3,12 +3,21 @@ import * as sourcegraph from 'sourcegraph'
 export function activate(): void {
    sourcegraph.search.registerQueryTransformer({
        transformQuery: (query: string) => {
-           const orgRegex = /\borg:(\w*)/
-           if (query.match(orgRegex)) {
-               const orgFilter = query.match(orgRegex)
-               const org = orgFilter && orgFilter.length >= 1 ? orgFilter[1] : ''
-               return query.replace(orgRegex  , `r:${org}/`)
+           const goImportsRegex = /\bgo.imports:([\w\/]*)/
+           if (query.match(goImportsRegex)) {
+               // Get package name
+               const pkgFilter = query.match(goImportsRegex)
+               const pkg = pkgFilter && pkgFilter.length >= 1 ? pkgFilter[1] : ''
 
+               // Package imported in grouped import statements
+               const matchPackage = '\\t\"' + pkg + '\"'
+               // Match packages with aliases
+               const matchAlias = '\\t(.*)\\s\"' + pkg + '\"'
+               // Match packages in single import statement
+               const matchSingle = 'import\\s\"' + pkg + '\"'
+               const finalRegex = `(${matchPackage}|${matchAlias}|${matchSingle})`
+
+               return query.replace(goImportsRegex  , finalRegex)
            }
            return query
         }
