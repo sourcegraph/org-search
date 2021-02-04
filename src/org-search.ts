d3805cdafe8278766103c2b4adf3b738d600fd17
Will Raschkowski <wraschkowski@palantir.com>
2021-02-04 14:23:42 +0000

    Add '-' to replacement regex

--- src/org-search.ts
+++ src/org-search.ts
@@ -3,7 +3,7 @@ import * as sourcegraph from 'sourcegraph'
 export function activate(): void {
    sourcegraph.search.registerQueryTransformer({
        transformQuery: (query: string) => {
-           const orgRegex = /\borg:(\w*)/
+           const orgRegex = /\borg:([\w-]*)/
            if (query.match(orgRegex)) {
                const orgFilter = query.match(orgRegex)
                const org = orgFilter && orgFilter.length >= 1 ? orgFilter[1] : ''
